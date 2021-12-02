5a11d1e484e907bf4fdfc1247907d6c7baad999e
Dasun Tharanga <dasuntharangaflash@gmail.com>
2021-12-03 00:37:31 +0530

    :white_check_mark: textit service added

---
+++ config/textit.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Dasun Tharanga ( @dasundev )
+ * Email: hello@dasun.dev
+ * Date: 2021-12-01
+ * Time: 9:22 PM
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | ID
+    |--------------------------------------------------------------------------
+    |
+    | The textit account ID that you need to authorize your account.
+    |
+    |
+    */
+
+    'id' => env('TEXTIT_ACCOUNT_ID', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password
+    |--------------------------------------------------------------------------
+    |
+    | The textit account password that you need to authorize your account.
+    |
+    |
+    */
+
+    'pw' => env('TEXTIT_ACCOUNT_PASSWORD', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Sender ID
+    |--------------------------------------------------------------------------
+    |
+    | Pre‐registered custom Sender ID (case sensitive)
+    |
+    |
+    */
+
+    'from' => env('TEXTIT_CUSTOM_SENDER_ID', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Economy Route
+    |--------------------------------------------------------------------------
+    |
+    | Selects economy route to send messages
+    |
+    |
+    */
+
+    'eco' => env('TEXTIT_ECO', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reply Mobile Number
+    |--------------------------------------------------------------------------
+    |
+    | Replies forwarded to the registered mobile number
+    |
+    |
+    */
+
+    'reply' => env('TEXTIT_REPLY_NUMBER', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reply URL
+    |--------------------------------------------------------------------------
+    |
+    | Replies will be posted to given url
+    |
+    |
+    */
+
+    'url' => env('TEXTIT_REPLY_URL', ''),
+];
---
+++ src/Textit.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Dasun Tharanga ( @dasundev )
+ * Email: hello@dasun.dev
+ * Date: 2021-12-01
+ * Time: 7:50 PM
+ */
+
+namespace Dasundev\LaravelTextit;
+
+use GuzzleHttp\Client;
+
+class Textit
+{
+    /**
+     * The textit client
+     *
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * Create a new Textit instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://www.textit.biz'
+        ]);
+    }
+
+    /**
+     * Send a message to the user
+     *
+     * @param $to
+     * @param $message
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function sendSMS($to, $message): string
+    {
+        $response = $this->client->get('sendmsg', [
+            'query' => [
+                'id' => config('textit.id'),
+                'pw' => config('textit.pw'),
+                'to' => $to,
+                'text' => $message
+            ]
+        ]);
+
+        $result = explode(":", (string) $response->getBody());
+
+        if (trim($result[0]) == "OK") {
+            return "Message sent - Result: " . $result[1];
+        } else {
+            return "Sent Failed - Error: " . $result[1];
+        }
+    }
+
+    /**
+     * Check textit account balance
+     *
+     * @return float
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function checkBalance(): float
+    {
+        $response = $this->client->get('creditchk', [
+            'query' => [
+                'id' => config('textit.id'),
+                'pw' => config('textit.pw')
+            ]
+        ]);
+
+        return (float) (string) $response->getBody();
+    }
+}
